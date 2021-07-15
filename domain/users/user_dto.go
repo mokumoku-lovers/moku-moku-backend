b8037f3a036111d8d599e7451bf722ef6b4563bf
Jiahui Chen <37770349+JiahuiChen99@users.noreply.github.com>
2021-07-15 17:07:50 +0200

    #3 feat: Email validation with regex

--- domain/users/user_dto.go
+++ domain/users/user_dto.go
@@ -1,5 +1,11 @@
 package users
 
+import (
+	"moku-moku/utils/errors"
+	"regexp"
+	"strings"
+)
+
 type User struct {
 	Email      string `json:"email"`
 	Username   string `json:"username"`
@@ -10,4 +16,24 @@ type User struct {
 	Password   string `json:"-"`
 	ProfilePic string `json:"profile_picture"`
 	Points     int32  `json:"points"`
+}
+
+func (user *User) EmailValidation() *errors.RestErr {
+	// Email sanitation
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+
+	if user.Email == "" {
+		return errors.BadRequest("invalid email address")
+	}
+
+	// Email validation
+	emailRegex := regexp.MustCompile(`^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$`)
+
+	validMail := emailRegex.MatchString(user.Email)
+
+	if !validMail {
+		return errors.BadRequest("invalid email address")
+	}
+
+	return nil
 }
