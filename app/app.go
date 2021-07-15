32bb28bf7d77bc4ff589ff9139fc2de3906e8f36
Jiahui Chen <37770349+JiahuiChen99@users.noreply.github.com>
2021-07-15 14:46:46 +0200

    feat: Starts default gin port 9000 and adds routing

--- app/app.go
+++ app/app.go
@@ -1,1 +1,23 @@
 package app
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+var (
+	router = gin.Default()
+)
+
+func StartApp() {
+	addRoutes()
+
+	err := router.Run(":9000")
+
+	if err != nil {
+		errMsg := fmt.Errorf("routing couldn't be set up")
+		fmt.Printf(errMsg.Error())
+		os.Exit(-1)
+	}
+}
