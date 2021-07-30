f026ff7d768e5783cf51db70bf46cc3b821b5529
JiahuiChen99 <Jiahui1@hotmail.es>
2021-07-30 13:03:11 +0200

    #15 feat: Insert Query and statement preparation

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,10 +2,14 @@ package users
 
 import (
 	"fmt"
+	"moku-moku/datasources/postgresql/users_db"
 	"moku-moku/utils/errors"
 )
 
 //User Data Access Object
+const (
+	queryInsertUser = "INSERT INTO users(email, username, display_name, biography, birthday, password, profile_pic, points, date_created) VALUES (?,?,?,?,?,?,?,?,?)"
+)
 
 var (
 	usersDB = make(map[int64]*User)
@@ -27,5 +31,13 @@ func (user *User) Get() *errors.RestErr {
 	user.ProfilePic = result.ProfilePic
 	user.Points = result.Points
 	user.DateCreated = result.DateCreated
+	return nil
+}
+
+func (user *User) Save() *errors.RestErr {
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+
+	defer stmt.Close()
+
 	return nil
 }
