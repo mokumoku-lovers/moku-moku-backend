8910632ce708c849cb7245e5d5f917813c1bdc98
Nicole Wert <nicalexxandra@gmail.com>
2021-08-06 22:23:59 +0900

    #6 feat: patch router mapping

--- app/routing.go
+++ app/routing.go
@@ -9,4 +9,5 @@ func addRoutes() {
 	router.POST("/users", users.CreateUser)
 	router.DELETE("/users/:user_id", users.DeleteUser)
 	router.PUT("/users/:user_id", users.UpdateUser)
+	router.PATCH("/users/:user_id", users.UpdateUser)
 }
