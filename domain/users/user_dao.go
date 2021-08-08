a0dc7996e4c4849d11eb00bb949a557da019f8ee
JiahuiChen99 <Jiahui1@hotmail.es>
2021-08-08 13:00:08 +0200

    #6 feat: COALESCE in SQL - Returns the data if it's not nil otherwise an empty string

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/sha256"
 	"encoding/hex"
+	"github.com/jackc/pgconn"
 	"moku-moku/datasources/postgresql/users_db"
 	"moku-moku/utils/date_utils"
 	"moku-moku/utils/errors"
@@ -17,7 +18,7 @@ import (
 //User Data Access Object
 const (
 	queryInsertUser = "INSERT INTO user_db.users(email, username, display_name, biography, birthday, password, profile_pic, points, date_created) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id;"
-	queryGetUser    = "SELECT id, email, username, display_name, biography, to_char(birthday, 'YYYY-MM-DD') AS birthday, password, profile_pic, points, to_char(date_created, 'YYYY-MM-DD') AS date_created FROM user_db.users WHERE id =$1;"
+	queryGetUser    = "SELECT id, email, username, display_name, biography, COALESCE(to_char(birthday, 'YYYY-MM-DD'), '') AS birthday, password, profile_pic, points, to_char(date_created, 'YYYY-MM-DD') AS date_created FROM user_db.users WHERE id =$1;"
 	queryDeleteUser = "DELETE FROM user_db.users WHERE id = $1;"
 	queryUpdateUser = "UPDATE user_db.users SET email=$2, username=$3, display_name=$4, biography=$5, birthday=$6, password=$7, profile_pic=$8, points=$9 WHERE id=$1;"
 )
