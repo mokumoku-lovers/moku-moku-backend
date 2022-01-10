08efe0bcf5401eb835069125f30d861d437fa852
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-10 20:48:53 +0100

    #58 feat: get user password query

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -20,24 +20,32 @@ import (
 //User Data Access Object
 const (
 	queryInsertUser                = "INSERT INTO user_db.users(email, username, display_name, biography, birthday, password, profile_pic, points, date_created) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id;"
-	queryGetUser                   = "SELECT id, email, username, display_name, biography, COALESCE(to_char(birthday, 'YYYY-MM-DD'), '') AS birthday, password, profile_pic, points, to_char(date_created, 'YYYY-MM-DD') AS date_created FROM user_db.users WHERE id =$1;"
-	queryDeleteUser                = "DELETE FROM user_db.users WHERE id = $1;"
+	queryGetUser                   = "SELECT id, email, username, display_name, biography, COALESCE(to_char(birthday, 'YYYY-MM-DD'), '') AS birthday, profile_pic, points, to_char(date_created, 'YYYY-MM-DD') AS date_created FROM user_db.users WHERE id=$1;"
+	queryGetUserPassword           = "SELECT password FROM user_db.users WHERE id=$1;"
+	queryDeleteUser                = "DELETE FROM user_db.users WHERE id=$1;"
 	queryUpdateUser                = "UPDATE user_db.users SET email=$2, username=$3, display_name=$4, biography=$5, birthday=$6, profile_pic=$7, points=$8 WHERE id=$1;"
 	queryUpdatePassword            = "UPDATE user_db.users SET password=$2 WHERE id=$1;"
 	queryGetUserByEmailAndPassword = "SELECT id, username, display_name, biography, COALESCE(to_char(birthday, 'YYYY-MM-DD'), '') AS birthday, profile_pic, points, to_char(date_created, 'YYYY-MM-DD') AS date_created FROM user_db.users WHERE email=$1 AND password=$2;"
 )
 
 func (user *User) Get() *errors.RestErr {
-	//err := users_db.Client.QueryRow(context.Background(), queryGetUser, user.Id).Scan(&user.Id, &user.Email, &user.Username, &user.DisplayName, &user.Biography, &user.Birthday, &user.Password, &user.ProfilePic, &user.Points, &user.DateCreated)
+	// Get user data
 	var users []*User
-	err := pgxscan.Select(context.Background(), users_db.Client, &users, queryGetUser, user.Id)
-	if err != nil {
+	if err := pgxscan.Select(context.Background(), users_db.Client, &users, queryGetUser, user.Id); err != nil {
 		return pg_utils.ParseError(err, "error when trying to get user")
 	}
+	// Get user password
+	var userPass = ""
+	if err := pgxscan.Get(context.Background(), users_db.Client, &userPass, queryGetUserPassword, user.Id); err != nil {
+		return pg_utils.ParseError(err, "error when trying to get user")
+	}
+
 	if users == nil {
 		return errors.NotFoundError("user doesn't exist")
 	}
 	*user = *users[0]
+	user.Passwords.Password = userPass
+
 	return nil
 }
 
